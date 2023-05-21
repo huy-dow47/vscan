9f35388d32927913ca58f46be474a8c503dd45bc
Burak <124401463+BUR-Ak-47@users.noreply.github.com>
2023-05-21 21:33:28 +0200

    Update vscan.py

--- vscan.py
+++ vscan.py
@@ -6,69 +6,71 @@ from http.cookies import SimpleCookie
 
 def display_ascii_art():
     ascii_art = """
- _____ _           _   _            _   _____           _             
-|  __ (_)         | | (_)          | | |_   _|         | |            
-| |__) | _ __   __| |  _  ___  ___ | |_  | |  _ __  ___| |_ ___  _ __ 
-|  ___/ | '_ \ / _` | | |/ _ \/ __|| __| | | | '_ \/ __| __/ _ \| '__|
-| |   | | | | | (_| | | |  __/\__ \| |_ _| |_| | | \__ \ || (_) | |   
-|_|   |_|_| |_|\__,_| |_|\___||___/ \__|_____|_| |_|___/\__\___/|_|   
+ __   __  _______  _______  _______  __    _ 
+|  | |  ||       ||       ||   _   ||  |  | |
+|  |_|  ||  _____||       ||  |_|  ||   |_| |
+|       || |_____ |       ||       ||       |
+|       ||_____  ||      _||       ||  _    |
+ |     |  _____| ||     |_ |   _   || | |   |
+  |___|  |_______||_______||__| |__||_|  |__|
+
     """
     print(ascii_art)
 
 def check_vulnerabilities(url, silent, checkall, ports, ssl_check, cookies, content, proxy, timeout, redirects):
-    print(f"Vérification des vulnérabilités pour : {url}")
+    print(f"Checking vulnerabilities for: {url}")
     print("-" * 50)
-    # Vérification des en-têtes de réponse
+    # Checking response headers
     if checkall:
         response = requests.head(url, proxies={"http": proxy, "https": proxy}, timeout=timeout)
-        print("En-têtes de réponse :")
+        print("Response Headers:")
         print(response.headers)
         print("-" * 50)
 
-    # Vérification des ports ouverts
+    # Checking open ports
     if ports:
-        print("Ports ouverts :")
+        print("Open Ports:")
         open_ports = get_open_ports(url)
         if open_ports:
             for port in open_ports:
-                print(f"Port {port} ouvert")
+                print(f"Port {port} open")
         else:
-            print("Aucun port ouvert trouvé.")
+            print("No open ports found.")
         print("-" * 50)
 
-    # Vérification des vulnérabilités SSL/TLS
+    # Checking SSL/TLS vulnerabilities
     if ssl_check:
-        print("Vérification des vulnérabilités SSL/TLS :")
+        print("Checking SSL/TLS vulnerabilities:")
         ssl_results = check_ssl_vulnerabilities(url)
         print(ssl_results)
         print("-" * 50)
 
-    # Vérification des cookies
+    # Checking cookies
     if cookies:
-        print("Vérification des cookies :")
+        print("Checking cookies:")
         cookie_results = check_cookies(url)
         print(cookie_results)
         print("-" * 50)
 
-    # Vérification du contenu de la page
+    # Checking page content
     if content:
-        print("Vérification du contenu de la page :")
+        print("Checking page content:")
         page_content = get_page_content(url)
         if page_content:
             print(page_content)
         else:
-            print("Aucun contenu trouvé.")
+            print("No content found.")
         print("-" * 50)
 
-    # Vérification des redirections
+    # Checking redirects
     if redirects:
-        print("Vérification des redirections :")
+        print("Checking redirects:")
         redirections = get_redirects(url)
         if redirections:
             for redirect in redirections:
-                print(f"Redirection : {redirect}")
+                print(f"Redirect: {redirect}")
         else:
-            print("Aucune redirection trouvée.")
+            print("No redirects found.")
         print("-" * 50)
 
 def get_open_ports(url):
@@ -95,7 +97,7 @@ def check_ssl_vulnerabilities(url):
         ssl_socket.close()
         return ssl_info
     except ssl.SSLError:
-        return "Une erreur SSL s'est produite."
+        return "An SSL error occurred."
 
 def check_cookies(url):
     try:
@@ -106,9 +108,9 @@ def check_cookies(url):
             cookie.load(cookies)
             return cookie
         else:
-            return "Aucun cookie trouvé."
+            return "No cookies found."
     except requests.exceptions.RequestException:
-        return "Une erreur s'est produite lors de la vérification des cookies."
+        return "An error occurred while checking cookies."
 
 def get_page_content(url):
     try:
@@ -125,17 +127,17 @@ def get_redirects(url):
         return []
 
 def main():
-    parser = argparse.ArgumentParser(description="Vérification des vulnérabilités d'un site")
-    parser.add_argument("-u", "--url", required=True, help="URL du site à vérifier")
-    parser.add_argument("-s", "--silent", action="store_true", help="Exécution silencieuse")
-    parser.add_argument("-c", "--checkall", action="store_true", help="Vérification complète")
-    parser.add_argument("--ports", action="store_true", help="Afficher les ports ouverts")
-    parser.add_argument("--ssl", action="store_true", help="Vérifier les vulnérabilités SSL/TLS")
-    parser.add_argument("--cookies", action="store_true", help="Vérifier les cookies")
-    parser.add_argument("--content", action="store_true", help="Vérifier le contenu de la page")
-    parser.add_argument("--proxy", help="Adresse du proxy (ex: http://proxy.example.com:8080)")
-    parser.add_argument("--timeout", type=int, default=10, help="Délai d'attente en secondes (par défaut: 10)")
-    parser.add_argument("--redirects", action="store_true", help="Vérifier les redirections")
+    parser = argparse.ArgumentParser(description="Check vulnerabilities of a website")
+    parser.add_argument("-u", "--url", required=True, help="URL of the website to check")
+    parser.add_argument("-s", "--silent", action="store_true", help="Silent execution")
+    parser.add_argument("-c", "--checkall", action="store_true", help="Perform complete check")
+    parser.add_argument("--ports", action="store_true", help="Display open ports")
+    parser.add_argument("--ssl", action="store_true", help="Check SSL/TLS vulnerabilities")
+    parser.add_argument("--cookies", action="store_true", help="Check cookies")
+    parser.add_argument("--content", action="store_true", help="Check page content")
+    parser.add_argument("--proxy", help="Proxy address (e.g., http://proxy.example.com:8080)")
+    parser.add_argument("--timeout", type=int, default=10, help="Timeout in seconds (default: 10)")
+    parser.add_argument("--redirects", action="store_true", help="Check redirects")
 
     args = parser.parse_args()
 
